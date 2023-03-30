ad64b1ab1adb0f591c452408bdc01d68948a8347
DragicevicS <steffan95@live.com>
2023-03-30 14:19:16 +0200

    Add switch grid and clear button functions

--- js.js
+++ js.js
@@ -1,12 +1,56 @@
 const container = document.querySelector('.grid-container');
-let gridNumber = 2;
+const divs = document.querySelectorAll('.grid-container');
+
+const popup = document.querySelector('.popup');
+
+
+let gridNumber = prompt("Enter grid size (min. 2, max. 100):");
+
+popup.addEventListener('click', () => {
+  location.reload();
+});
+
 let divWidth = 600 / gridNumber;
 let divHeight = 600 / gridNumber;
 
-for (let i=1; i <= gridNumber * gridNumber; i++) {
-  const div = document.createElement('div');
-  div.classList.add(i);
-  div.style.minWidth = divWidth + 'px';
-  div.style.minHeight = divHeight + 'px';
-  container.appendChild(div);
+let color = document.querySelectorAll('.color');
+let divColor = 'black';
+color.forEach((color) => {
+  color.addEventListener('click', () => {
+    divColor = color.getAttribute('id');
+  });
+});
+
+function setGrid(num) {
+  for (let i=1; i <= num * num; i++) {
+    const div = document.createElement('div');
+    div.classList.add(i);
+    div.style.minWidth = divWidth + 'px';
+    div.style.minHeight = divHeight + 'px';
+    div.style.backgroundColor = '#bfbfbf'
+    divs.forEach((divs) => {
+      divs.addEventListener('mouseover', e => {
+        e.target.style.backgroundColor = divColor ;
+      });
+    });
+    const clear = document.querySelector('.clear');
+    clear.addEventListener('click', () => {
+      div.style.backgroundColor = '#bfbfbf';
+    });
+    container.appendChild(div);
+  };
 };
+
+if (gridNumber > 1 && gridNumber < 101) setGrid(gridNumber);
+else location.reload();
+
+
+
+
+
+
+
+
+
+
+
